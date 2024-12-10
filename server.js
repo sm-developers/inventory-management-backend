b268af121167d380c2a90f745ce42bf7378ec602
shouryamanekar <speedk143@gmail.com>
2024-12-11 01:34:03 +0530

    Add routes for user authentication

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+const authRoutes = require('./src/routes/authRoutes');
+
 dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Inventory Management Backend is Running!');
